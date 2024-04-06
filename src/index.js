1cc3945b5f67adead7b3cff4db0a37c865fb26f0
Zil Chaudhary <48820842+ZilChaudhary@users.noreply.github.com>
2024-04-06 14:19:50 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { App } from './App';
+
+const rootElement = document.getElementById('root');
+const isStrictMod = false;
+
+function AppStrict() {
+  return isStrictMod ? (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  ) : (
+    <App />
+  );
+}
+
+ReactDOM.render(<AppStrict />, rootElement);
